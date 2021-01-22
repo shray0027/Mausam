901811b43fa24ad88d7d0f571c6825d13db787b5
shray0027 <59202921+shray0027@users.noreply.github.com>
2021-01-22 10:04:42 +0530

    checking

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,4 @@
 console.log("hello js");
-// AIzaSyCmJVIuZLj8MUU6DtEGVb48YruRvMuKpv0
 const weatherForm = document.querySelector('form');
 const search=document.querySelector('input');
 const temp=document.querySelector('#temp');
@@ -13,8 +12,9 @@ const long1=document.querySelector('#long');
 const desc=document.querySelector('#desc');
 const map_ =document.getElementById("map");
 const img1 =document.querySelector(".img-1");
+const btn =document.querySelector(".search");
 
-weatherForm.addEventListener('submit',(e)=>{
+btn.addEventListener('click',(e)=>{
     e.preventDefault();
     temp.textContent='loding...';
     loc.textContent='';
@@ -23,30 +23,24 @@ weatherForm.addEventListener('submit',(e)=>{
     long1.textContent = '';
     msg.textContent='';
     fetch('https://weather-blitz.herokuapp.com/weather?address='+search.value).then((res,error)=>{
-    if(error){
-        console.log("shray");
-    }    
-   
     res.json().then((data)=> {
         console.log(data);
             if(data.error){
             errorImg.src='/img/address404.jpg';
             msg.textContent=data.error;
-            console.log(msg.textContent);
             errorMsg.style.display="flex";
             content.style.display="none";
             map_.style.display="none";
             } else {
-                errorMsg.style.display="none";
-                content.style.display="flex";
-                temp.textContent= "Temperature : "+data.temperature;
-                loc.textContent="Place : "+data.location;
+                 errorMsg.style.display="none";
+                 content.style.display="flex";
+                 temp.textContent= "Temperature : "+data.temperature;
+                 loc.textContent="Place : "+data.location;
                  desc.textContent=data.description;
                  lat1.textContent = "Lat - "+ data.latitude+" ,";
                  long1.textContent ="Long - " +data.longitude;
                  img1.src = data.weatherIcon;
-                console.log("hello") ;
-                map_.style.display="block";         
+                 map_.style.display="block";         
             }
         });
     });
